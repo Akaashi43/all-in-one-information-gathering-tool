bbe4a189d824b4247709021ed74cd6d69f41a386
@Akash_Prajapati <118038514+Akaashi43@users.noreply.github.com>
2025-03-05 00:50:47 +0530

    Add files via upload

---
+++ help.sh
@@ -0,0 +1,7 @@
+echo " "
+echo " Now  Swith to Super/Root user ( sudo su) "
+echo " run the command  ./allrecon6.py or bash allrecon6.py "
+echo " "
+
+echo " give domain/IP and choose selective number to perform scan "
+
---
+++ setup.sh
@@ -0,0 +1,28 @@
+sudo apt update -y
+sudo apt install sublist3r
+sudo apt install -y whois nmap dnsenum sqlmap assetfinder curl git python3-pip
+
+# pip install colorama
+
+sudo apt install finalrecon -y
+sudo apt install whatweb -y
+sudo apt install recon-ng -y
+sudo apt install amass -y
+sudo apt install assetfinder -y
+
+
+echo " all instaltion done "
+echo " all instaltion done "
+echo " all instaltion done "
+echo " all instaltion done "
+echo " all instaltion done "
+echo " "
+echo " "
+echo " Too see help page run command  "
+echo "      ./help.sh   or   bash help.sh " 
+echo " "
+echo " Now  Swith to Super/Root user ( sudo su) "
+echo " run the command  ./allrecon6.py  or  bash allrecon6.py  "
+echo " "
+
+echo " give domain/IP and choose selective number to perform scan "
